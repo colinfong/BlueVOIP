87326605b077f5b884fda867d591378834648819
achen93 <angelchen@Angels-MacBook-Pro.local>
2016-05-17 15:41:00 -0700

    Fixed repository

--- app/src/main/java/com/example/chulheewoo/a117/MainActivity.java
+++ app/src/main/java/com/example/chulheewoo/a117/MainActivity.java
@@ -38,7 +38,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class MainActivity extends Activity {
+import java.io.File;
+import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int DISCOVER_DURATION = 300;
+    private static final int REQUEST_BLU = 1;
+
     private BluetoothAdapter BA;
     private Set<BluetoothDevice> pairedDevices;
     ListView lv;
@@ -59,30 +68,28 @@ public class MainActivity extends Activity {
         BA = BluetoothAdapter.getDefaultAdapter();
         lv = (ListView) findViewById(R.id.listView);
         /****************************/
-
+        //Button assignments for audio playback
         play = (Button) findViewById(R.id.play);
         stop = (Button) findViewById(R.id.stop);
         record = (Button) findViewById(R.id.record);
 
         stop.setEnabled(false);
         play.setEnabled(false);
+        //Choose the destination for the audio recording in the SD card
         outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
-        /*
-        myAudioRecorder = new MediaRecorder();
-        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        myAudioRecorder.setOutputFile(outputFile);
-        */
+
+        //Assign record button click action
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*create MediaRecorder to declare audio source, output format, and audio encoding*/
                 myAudioRecorder = new MediaRecorder();
                 myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                 myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
                 myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
                 myAudioRecorder.setOutputFile(outputFile);
                 try {
+                    //Prepare recording device by getting its resources
                     myAudioRecorder.prepare();
                     myAudioRecorder.start();
                 } catch (IllegalStateException e) {
@@ -96,34 +103,44 @@ public class MainActivity extends Activity {
                 //record.setEnabled(false);
                 stop.setEnabled(true);
 
+                //Notify user of recording
                 Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
             }
         });
 
+        //Declare stop button function
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myAudioRecorder.stop();
+                //releases the audio recording resources
                 myAudioRecorder.release();
                 myAudioRecorder = null;
 
+                //Disallow stop from being clicked again
                 stop.setEnabled(false);
+                //Enable the playback of the recorded audio
                 play.setEnabled(true);
 
+                //Announce the reodring of the audio
                 Toast.makeText(getApplicationContext(), "Audio recorded successfully", Toast.LENGTH_LONG).show();
             }
         });
 
+        //Assign action to play button
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) throws IllegalArgumentException, SecurityException, IllegalStateException {
+                //Create a media player
                 MediaPlayer m = new MediaPlayer();
 
+                //Try to play the recorded file
                 try {
                     m.setDataSource(outputFile);
                   } catch (IOException e) {
                     e.printStackTrace();
                 }
+
 
                 try {
                     m.prepare();
@@ -141,6 +158,95 @@ public class MainActivity extends Activity {
     }
 
     /*******************************************************************************************************/
+
+    // FILE SEND OVER BLUETOOTH
+
+    public void sendViaBluetooth(View v) {
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(btAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_LONG).show();
+        } else {
+            enableBluetooth();
+        }
+    }
+
+    public void enableBluetooth() {
+
+        Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+
+        discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVER_DURATION);
+
+        startActivityForResult(discoveryIntent, REQUEST_BLU);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode == DISCOVER_DURATION && requestCode == REQUEST_BLU) {
+
+            String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+            String myFile = "download.jpg";
+            //String path= baseDir + "/Download/" + myFile;
+            //String path = "file:///storage/sdcard0/Download/" + myFile;
+            String path = "file://" + Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+            //if(path.startsWith("file")||path.startsWith("content")||path.startsWith("FILE")||path.startsWith("CONTENT")){
+
+            //}else{
+            //    path="file://"+path;
+            //}
+            /*
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path));
+            shareIntent.setType("video/mp4");
+            startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.sendTo)));
+            */
+
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            //intent.setType("image/jpeg");
+            intent.setType("audio/3gpp");
+            //File f = new File(Environment.getExternalStorageDirectory(), "31286.txt");
+            //intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(f));
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(path));
+
+            PackageManager pm = getPackageManager();
+            List<ResolveInfo> appsList = pm.queryIntentActivities(intent, 0);
+
+            if(appsList.size() > 0) {
+                String packageName = null;
+                String className = null;
+                boolean found = false;
+
+                for(ResolveInfo info : appsList) {
+                    packageName = info.activityInfo.packageName;
+                    if(packageName.equals("com.android.bluetooth")) {
+                        className = info.activityInfo.name;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    Toast.makeText(this, "Bluetooth havn't been found",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    intent.setClassName(packageName, className);
+                    startActivity(intent);
+                }
+            }
+        } else {
+            Toast.makeText(this, "Bluetooth is cancelled", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    /*************************************************************************************************/
+
+    // BLUETOOTH CONNECTION AUX FXNS
+
     public void on(View v) {
         if (!BA.isEnabled()) { //Bluetooth adapter is not enabled
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -172,6 +278,7 @@ public class MainActivity extends Activity {
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         lv.setAdapter(adapter);
     }
+    /*************************************************************************************************/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
